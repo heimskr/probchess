8181e5efb3c02b8c1ea916fb0907813d336e63e2
Kai Tamkun <ktamkun@ucsc.edu>
2020-09-17 23:21:44 -0700

    Rook movement

--- server/src/piece/Rook.cpp
+++ server/src/piece/Rook.cpp
@@ -1,5 +1,49 @@
+#include "Board.h"
 #include "piece/Rook.h"
 
 std::list<Square> Rook::canMoveTo() {
-	return {};
+	std::list<Square> out;
+
+	Square next = square;
+	for (int i = 0; i < 7; ++i) { // up
+		next -= 1;
+		if (!next)
+			break;
+		out.push_back(square);
+		if (parent->at(square))
+			break;
+	}
+
+	next = square;
+	for (int i = 0; i < 7; ++i) { // down
+		next += 1;
+		if (!next)
+			break;
+		out.push_back(square);
+		if (parent->at(square))
+			break;
+	}
+
+	next = square;
+	for (int i = 0; i < 7; ++i) { // left
+		next <<= 1;
+		if (!next)
+			break;
+		out.push_back(square);
+		if (parent->at(square))
+			break;
+	}
+
+	next = square;
+	for (int i = 0; i < 7; ++i) { // right
+		next >>= 1;
+		if (!next)
+			break;
+		out.push_back(square);
+		if (parent->at(square))
+			break;
+	}
+
+	filter(out);
+	return out;
 }
