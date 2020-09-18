415d7b1c6ee5e9939b7c47f6debd6dea2a4b29a2
Kai Tamkun <ktamkun@ucsc.edu>
2020-09-17 23:20:00 -0700

    Knight movement

--- server/src/piece/Knight.cpp
+++ server/src/piece/Knight.cpp
@@ -1,5 +1,23 @@
 #include "piece/Knight.h"
 
 std::list<Square> Knight::canMoveTo() {
-	return {};
+	std::list<Square> out;
+
+	// . X . X .
+	// X . . . X
+	// . . x . .
+	// X . . . X
+	// . X . X .
+
+	out.push_back(square + std::make_pair(-2, -1));
+	out.push_back(square + std::make_pair(-2,  1));
+	out.push_back(square + std::make_pair(-1,  2));
+	out.push_back(square + std::make_pair( 1,  2));
+	out.push_back(square + std::make_pair( 2,  1));
+	out.push_back(square + std::make_pair( 2, -1));
+	out.push_back(square + std::make_pair( 1, -2));
+	out.push_back(square + std::make_pair(-1, -2));
+
+	filter(out);
+	return out;
 }
