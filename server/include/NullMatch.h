dcf30c407c8c76282d92e57aefbc02072a3cd443
Kai Tamkun <ktamkun@ucsc.edu>
2020-10-14 22:49:53 -0700

    Allow black null matches

--- server/include/NullMatch.h
+++ server/include/NullMatch.h
@@ -8,7 +8,9 @@
 class NullMatch: public Match {
 	public:
 		NullMatch(const std::string &id_, bool hidden_, bool no_skip, int column_count, Color host_color):
-			Match(id_, hidden_, no_skip, column_count, host_color) {}
+			Match(id_, hidden_, no_skip, column_count, host_color) {
+			currentTurn = host_color;
+		}
 
 		~NullMatch() {}
 
